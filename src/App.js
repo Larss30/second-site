ab2b169566acd2fec19f959b6cd92f298bb73737
Larss30 <larencearroz@gmail.com>
2023-07-03 01:00:02 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import { Blog, Footer, Possibility, Features, Header, WhatGPT3 } from './Containers'
-import { CTA, Brand, Navbar } from './Components'
+import logo from './logo.svg';
+import './App.css';
 
-
-const App = () => {
+function App() {
   return (
-    <div className='App gradient_bg'>
-      <div>
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
