e256405a3b676947ae05860bd95a43595f5d920c
Larss30 <larencearroz@gmail.com>
2023-07-03 10:31:52 +0800

    test

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,15 +2,9 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = ({title, price, subtext}) => (
-  <div className='app__menuItem'>
-    <div className='app__menuItem-title'>
-      <h4 className='title headtext__cormorant'>{title}</h4>
-      <div className='app__menuItem-title-sep'/>
-      <h5 className='price headtext__cormorant'>{price}</h5>
-    </div>
-    <p className='p__opensans subtext'>{subtext}</p>
-    
+const MenuItem = () => (
+  <div>
+    MenuItem
   </div>
 );
 
--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import images from '../../constants/images'
 
-const SubHeading = ({title}) => (
-  <div style={{marginBottom: '1rem'}}>
-    <p className='p__cormorant'>{title}</p>
-    <img src={images.spoon} alt='spoon image' className='spoon__img'/>
+const SubHeading = () => (
+  <div>
+    SubHeading
   </div>
 );
 
